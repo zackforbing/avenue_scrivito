401c60121193b6ee7e733239ae19b597cde5965a
Zack Forbing <zackforbing@gmail.com>
2017-05-08 14:38:32 -0600

    add JS file to show navbar while in edit function

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,5 @@
+$(document).ready( function() {
+  if (scrivito.in_editable_view()) {
+    $(".navbar").removeClass("navbar-fixed-top")
+  }
+})
--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -1,6 +1,5 @@
 class HomePage < Obj
   attribute :title, :string
-  attribute :image, :reference
   attribute :body, :widgetlist
   attribute :child_order, :referencelist
   attribute :tags, :stringlist
