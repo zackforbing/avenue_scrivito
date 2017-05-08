5609b5d0af2e9661e731cea58d1d9c78f4f98e5b
Caleb <calebcowen@gmail.com>
2017-05-08 14:47:35 -0600

    Add editable logo to navbar

--- app/models/home_page.rb
+++ app/models/home_page.rb
@@ -1,5 +1,6 @@
 class HomePage < Obj
   attribute :title, :string
+  attribute :logo, :reference
   attribute :body, :widgetlist
   attribute :child_order, :referencelist
   attribute :tags, :stringlist
