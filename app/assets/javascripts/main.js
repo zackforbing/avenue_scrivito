5b5a3f9d7cdcac41849272f1ce7bd4627d390bb9
Caleb <calebcowen@gmail.com>
2017-05-08 18:28:31 -0600

    fix all the things

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,24 @@ $(document).ready( function() {
   if (scrivito.in_editable_view()) {
     $(".navbar").removeClass("navbar-fixed-top")
   }
+  hideNavbar();
 })
+
+function hideNavbar() {
+  var lastScroll = 0;
+  $(window).scroll(function(event){
+      //Sets the current scroll position
+      var st = $(this).scrollTop();
+      //Determines up-or-down scrolling
+      if (st > lastScroll){
+         //Replace this with your function call for downward-scrolling
+         $(".navbar").hide();
+      }
+      else {
+         //Replace this with your function call for upward-scrolling
+         $(".navbar").show();
+      }
+      //Updates scroll position
+      lastScroll = st;
+  });
+}
